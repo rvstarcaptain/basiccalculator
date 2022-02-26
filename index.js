bab86bf1f9bb250a3521bf37208713ba30a8136f
Rahul-Verma <94426476+starcaptian@users.noreply.github.com>
2022-02-26 14:15:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+function calculate(){
+    console.log("print calculate");
+    let num1=document.getElementById('num1').value;
+    let num2=document.getElementById('num2').value;
+    
+    num1 = Number(num1);
+    num2 = Number(num2);
+    if(!num1 == !num2){
+        alert("please enteer the  valid number!!!!");
+    }
+
+    let operation=document.getElementById("operation").value;
+    
+    let answer;
+    if(operation=="add"){
+        answer=add(num1,num2);      
+    }
+    else if(operation == "subtract"){
+        answer = subtract(num1,num2);
+    }
+    else if(operation=="multiply"){
+        answer= multiply(num1,num2);
+    }
+    else{
+        answer = divide(num1,num2);
+    }
+    
+    document.getElementById("calculator-output").innerHTML=answer;
+    
+}
+
+
+function add(a,b){
+    return a+b;
+}
+function subtract(num1,num2){
+    return num1-num2;
+}
+function multiply(num1,num2){
+    return num1*num2;
+}
+function divide(num1,num2){
+    return num1/num2;
+}
