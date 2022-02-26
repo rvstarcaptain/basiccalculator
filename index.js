2083591ff65bcc0b82e4a01af865648cda7183e2
Rahul-Verma <94426476+starcaptian@users.noreply.github.com>
2022-02-26 14:18:14 +0530

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function calculate(){
     
     num1 = Number(num1);
     num2 = Number(num2);
-    if(!num1 == !num2){
+    if(!num1 || !num2){
         alert("please enteer the  valid number!!!!");
     }
 
